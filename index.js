d773f64e6090bd2a3f3702e3f36371aeecd19252
yovercc <ycastroc@uni.pe>
2023-05-20 17:26:47 -0500

    Get methods

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000
+    },
+    {
+      name: 'Product 2',
+      price: 2000
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000
+  })
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset){
+    res.json({
+      limit,
+      offset
+    });
+  }else{
+    res.send('No hay parametros');
+  }
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+ const { categoryId, productId } = req.params;
+ res.json({
+  categoryId,
+  productId
+})
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

0287bec60e893c2ed28f4615f145b09c6755943c
yovercc <ycastroc@uni.pe>
2023-06-11 17:36:24 -0500

    Migraciones y Relaciones uno a uno

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -6,15 +6,16 @@ const setupModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
+  dialect: 'postgres',
   logging: console.log,
 });
 
 setupModels(sequelize);
 
-sequelize.sync(); // va a coger los modelos y va a crear la estructura
+// sync() no se aconseja para correr en produccion. leer la documentacion
+//sequelize.sync(); // va a coger los modelos y va a crear la estructura
 
 module.exports = sequelize;
